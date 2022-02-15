863d6567e1ebf1c62512b81dbf4d2d24da1b6ee7
Thien Dang <ctap105@gmail.com>
2022-02-15 12:37:15 -0500

    models update

--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    recipes = db.relationship("Recipe", back_populates='user', cascade='all, delete-orphan')
+    reviews = db.relationship("Review", back_populates='user', cascade='all, delete-orphan')
+
     @property
     def password(self):
         return self.hashed_password
@@ -27,4 +30,84 @@ class User(db.Model, UserMixin):
             'id': self.id,
             'username': self.username,
             'email': self.email
+        }
+
+
+class Recipe(db.model):
+    __tablename__ = "recipes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    time_updated = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    user = db.relationship("User", back_populates="recipes")
+    reviews = db.relationship("Review", back_populates="recipe", cascade='all, delete-orphan')
+    ingredients = db.relationship("Ingredient", back_populates='recipe', cascade='all, delete-orphan')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'user_id': self.user_id,
+            'time_created': self.time_created,
+            'time_updated': self.time_updated,
+        }
+
+
+class Review(db.model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"))
+    time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    time_updated = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    user = db.relationship("User", back_populates="reviews")
+    recipe = db.relationship("Recipe", back_populates="reviews")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'rating': self.rating,
+            'review': self.review,
+            'user_id': self.user_id,
+            'recipe_id': self.recipe_id,
+            'time_created': self.time_created,
+            'time_updated': self.time_updated,
+        }
+
+
+class Category(db.model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+class Ingredient(db.model):
+    __tablename__ = 'ingredients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"))
+
+    recipe = db.relationship("Recipe", back_populates='ingredients')
+
+    def to_dic(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            "recipe_id": self.recipe_id
         }
