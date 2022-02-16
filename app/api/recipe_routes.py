0b29dbe9fe93e047ce153481151b00938d8f3716
Thien Dang <ctap105@gmail.com>
2022-02-16 16:41:49 -0500

    change user state

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -21,7 +21,7 @@ def all_recipes():
     recipe_data = Recipe.query \
                                 .options(joinedload(Recipe.ingredients)) \
                                 .options(joinedload(Recipe.user)) \
-                                .options(joinedload(Recipe.reviews)) \
+                                .options(joinedload(Recipe.reviews).options(joinedload(Review.user))) \
                                 .options(joinedload(Recipe.categories)) \
                                 .options(joinedload(Recipe.photos)) \
                                 .all()
@@ -30,7 +30,8 @@ def all_recipes():
 
     for recipe in recipe_data:
         ingredients = [ingredient.to_dict() for ingredient in recipe.ingredients]
-        reviews_set = [review.to_dict() for review in recipe.reviews]
+        # reviews_set = [review.to_dict() for review in recipe.reviews]
+        review_set = get_reviews(recipe.reviews)
         category_set = [category.to_dict() for category in recipe.categories]
         photo_set = [photo.to_dict() for photo in recipe.photos]
 
@@ -44,10 +45,41 @@ def all_recipes():
             "time_update": recipe.time_updated,
             "user": recipe.user.to_dict(),
             "ingredients": ingredients,
-            "reviews": reviews_set,
+            "reviews": review_set,
             "categories": category_set,
             "photos": photo_set
         }
 
         data.append(each)
     return {"data": data}
+
+
+def get_reviews(reviews):
+    data =[]
+    for review in reviews:
+        user_data = get_user(review.user)
+
+        review_set = {
+            'id': review.id,
+            'rating': review.rating,
+            "recipe_id": review.recipe_id,
+            "review": review.review,
+            "time_created": review.time_created,
+            "time_updates": review.time_updated,
+            "user": user_data
+        }
+
+        data.append(review_set)
+    return data
+
+
+def get_user(user):
+    data = []
+
+    user_set = {
+            "id": user.id,
+            "username": user.username
+        }
+
+    data.append(user_set)
+    return data
--- react-app/src/components/app/HomeApp/SingleRecipe/index.js
+++ react-app/src/components/app/HomeApp/SingleRecipe/index.js
@@ -18,10 +18,16 @@ const SingleRecipe = () => {
 
     let rating = 0;
     const ratings = target?.reviews?.map(review => review.rating)
+
     if (ratings?.length) {
         ratings?.forEach( rate => rating = rate + rating)
         rating = rating / ratings.length
     }
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+      }, [])
+
 
     useEffect(() => {
         (async() => {
@@ -51,6 +57,35 @@ const SingleRecipe = () => {
             <div className='cat-reviews'>
                 <div className='single-cat'><span className='single-span'>Category:</span> <Link className='single-link' to={`/categories/${target.categories[0].name}`}>{target.categories[0].name}</Link></div>
                 <div className='single-stars'><span className="stars" style={{"--rating": `${rating}`}}></span></div>
+            </div>
+            <div className='butt-section'>
+                <div>
+                <h2 className='single-h2'>Ingredients:</h2>
+                    <ul>
+                    {target.ingredients.map((ingredient, idx) => (
+                        <li key={idx}>{ingredient.name}</li>
+                    ))}
+                    </ul>
+                </div>
+                <div>
+                    <div className='cooking-instructions'>
+                        <h2 className='single-h2'>Cooking Instructions:</h2>
+                        <p>{target.instructions}</p>
+                    </div>
+                    <div className='reviews'>
+                    <h2 className='single-h2'>Reviews:</h2>
+                        {target.reviews.map((review, idx) => (
+                            <div className='one-review'>
+                            <p key={idx}>{review.review}</p>
+                                <div className='id-review'>
+                                    {user.id === review.user_id ? <button className='single-butts'>Edit</button>:<></>}
+                                    {user.id === review.user_id ? <button className='single-butts'>Delete</button>:<></>}
+                                    <span className="stars" style={{"--rating": `${review.rating}`}}></span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     )
