df3ecc980896bfdd1cd0246abe18df252be5c3b3
Thien Dang <ctap105@gmail.com>
2022-02-17 08:50:37 -0500

    updated seed

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -12,31 +12,31 @@ def seed_reviews():
     db.session.add(ramen_review3)
 
     # Cereal
-    cereal_review1 = Review(rating=5, review='What can I say? This is Honey Nut Cheerios with nanners. It is the best.', recipe_id=2, user_id=1)
-    cereal_review2 = Review(rating=5, review='I can eat this stuff all day. Takes less than a minute to get together.', recipe_id=2, user_id=2)
-    cereal_review3 = Review(rating=4, review="I would have given this recipe five stars, but it should have used near rotten bananas instead.", recipe_id=2, user_id=3)
+    cereal_review1 = Review(rating=5, review='What can I say? This is Honey Nut Cheerios with nanners. It is the best.', recipe_id=3, user_id=1)
+    cereal_review2 = Review(rating=5, review='I can eat this stuff all day. Takes less than a minute to get together.', recipe_id=3, user_id=2)
+    cereal_review3 = Review(rating=4, review="I would have given this recipe five stars, but it should have used near rotten bananas instead.", recipe_id=3, user_id=3)
 
     db.session.add(cereal_review1)
     db.session.add(cereal_review2)
     db.session.add(cereal_review3)
 
     # Grilled Cheese
-    ch_review1 = Review(rating=5, review='This is grilled cheese. How can you go wrong?', recipe_id=3, user_id=1)
-    ch_review2 = Review(rating=1, review='This recipe is so hard to follow. I cannot tell when cheese is almost bad, and I have eaten mold one too many times because of this.', recipe_id=3, user_id=2)
+    ch_review1 = Review(rating=5, review='This is grilled cheese. How can you go wrong?', recipe_id=4, user_id=1)
+    ch_review2 = Review(rating=1, review='This recipe is so hard to follow. I cannot tell when cheese is almost bad, and I have eaten mold one too many times because of this.', recipe_id=4, user_id=2)
 
     db.session.add(ch_review1)
     db.session.add(ch_review2)
 
     # Spamich
-    spam_review1 = Review(rating=5, review='Spam and bread. That is all I need to get my day going. One day, I will be able to afford to put cheese on this.', recipe_id=4, user_id=1)
-    spam_review2 = Review(rating=4, review='Spam is good. Bread is good. Spam and bread together? That is just awesome.', recipe_id=4, user_id=3)
+    spam_review1 = Review(rating=5, review='Spam and bread. That is all I need to get my day going. One day, I will be able to afford to put cheese on this.', recipe_id=5, user_id=1)
+    spam_review2 = Review(rating=4, review='Spam is good. Bread is good. Spam and bread together? That is just awesome.', recipe_id=5, user_id=3)
 
     db.session.add(spam_review1)
     db.session.add(spam_review2)
 
     # Vienna Bread
-    ss_review1 = Review(rating=3, review='This recipe is a bit lack luster. It could really use a sauce.', recipe_id=5, user_id=1)
-    ss_review2 = Review(rating=2, review='Vienna Sausages are great by themselves. Why ruin them with bread? Come on, OP!', recipe_id=5, user_id=2)
+    ss_review1 = Review(rating=3, review='This recipe is a bit lack luster. It could really use a sauce.', recipe_id=2, user_id=1)
+    ss_review2 = Review(rating=2, review='Vienna Sausages are great by themselves. Why ruin them with bread? Come on, OP!', recipe_id=2, user_id=2)
 
     db.session.add(ss_review1)
     db.session.add(ss_review2)
