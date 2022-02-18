10725d14a2518185935082bed5875db4b3a940ef
Thien Dang <ctap105@gmail.com>
2022-02-18 11:47:31 -0500

    changed featured to have singular word for review

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -15,6 +15,7 @@ const SignUpForm = () => {
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
+      setErrors([])
       const data = await dispatch(signUp(username, email, password));
       if (data) {
         setErrors(data)
