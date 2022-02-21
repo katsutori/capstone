8b9fa3fceeb992e268ed97eb091523f955c398d5
Thien Dang <ctap105@gmail.com>
2022-02-21 10:45:47 -0500

    fixed map index error

--- react-app/src/components/app/HomeApp/SingleRecipe/index.js
+++ react-app/src/components/app/HomeApp/SingleRecipe/index.js
@@ -127,7 +127,7 @@ const SingleRecipe = () => {
                     </div>
                     <div>
                     {ingredientSet?.map((one, idx) => (
-                        <>
+                        <div key={idx}>
                             <div className='ingredients-container' key={idx}>
                                 <div className='ing-name'>{one.name}</div>
                                 <div className='ing-butt-cont'>
@@ -137,7 +137,7 @@ const SingleRecipe = () => {
                                 </div>
                             </div>
                             {working === true && ingredientEditing === idx && user.id === target.user_id ? <div><EditIngredientForm ingredientId={one.id} ingredientName={one.name}/></div>:<></>}
-                        </>
+                        </div>
                     ))}
                     <div className='ingredients-container-add'>
                         {target.user_id === user.id ? <AddIngredientForm />:<></>}
