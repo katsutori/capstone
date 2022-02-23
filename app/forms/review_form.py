adb3453617954fec08d6e6798e87e8e77bae2c51
Thien Dang <ctap105@gmail.com>
2022-02-22 21:03:13 -0500

    add rating validation

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -2,7 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, SubmitField, ValidationError
 from wtforms.validators import DataRequired
 
+def rating_check(form, field):
+    rating = field.data
+    if rating < 1 or rating > 5:
+        raise ValidationError('Please provide a rating between 1 and 5.')
+
 class ReviewForm(FlaskForm):
-    rating = IntegerField('rating', validators=[DataRequired("Please provide a rating between 1 and 5.")])
+    rating = IntegerField('rating', validators=[DataRequired("Please provide a rating between 1 and 5."), rating_check])
     review = StringField('review', validators=[DataRequired("Please provide a message for your review.")])
     submit = SubmitField('submit')
